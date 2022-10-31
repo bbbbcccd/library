8f554e4cbc8cf9bd5edd062d2e24a5bf2cd37657
Zac <zacheryngky@gmail.com>
2022-10-31 16:47:35 +0800

    Create functions to add and remove book from myLibrary

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function Book(title, author, pages, isRead) {
 Book.prototype.changeRead = function() {
     this.isRead = !this.isRead;
 }
+
+// Add book to last index of myLibrary
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+// Remove book from myLibrary based on its index
+function removeBookFromLibrary(index) {
+    myLibrary.splice(index, 1);
+}
+
+// Creating a book and adding it to myLibrary
+const book1 = new Book("Book1", "Book1", 1, false);
+addBookToLibrary(book1);
