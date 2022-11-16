7f388088b02173f0bce5eb57b965fcfd43449288
Zac <zacheryngky@gmail.com>
2022-11-16 13:03:04 +0800

    Replace book constructor with class syntax

--- script.js
+++ script.js
@@ -6,18 +6,20 @@ const formBtn = document.querySelector("#form-btn");
 
 let myLibrary = [];
 
-// Book constructor and prototype
-function Book(title, author, pages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
-}
+// Book class
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
 
-// Toggle isRead property
-Book.prototype.changeRead = function() {
-    this.isRead = !this.isRead;
-    renderBooks();
+    // Toggle isRead property and display on screen
+    changeRead() {
+        this.isRead = !this.isRead;
+        renderBooks();
+    }
 }
 
 // Render myLibrary on screen
